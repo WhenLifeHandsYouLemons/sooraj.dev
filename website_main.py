8f0e5a1a07fb1d85b674300fb56b2f4f10184223
Sooraj Sannabhadti <You'veGotToMakeLemonade@users.noreply.github.com>
2020-12-09 13:18:24 +0800

    Update website_main.py

--- website_main.py
+++ website_main.py
@@ -13,5 +13,5 @@ def main():
 def page2():
     return ("<h1>This is page 2!</h1>")
 
-app.run(host="127.0.0.1", port=8080, debug=True)
+app.run(host="0.0.0.0", port=8080, debug=True)
 #app.run(host='localhost', port=8080, debug=True)
