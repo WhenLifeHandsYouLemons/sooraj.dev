cb0e7b786e95d3942841dba6af364fc4ddb2007c
Sooraj Sannabhadti <2005sooraj@gmail.com>
2024-06-02 14:03:14 +0800

    Renamed folders and fixed more issues
    - Closed #19 (finally)

--- assets/js/art-transition.js
+++ assets/js/art-transition.js
@@ -1,19 +1,19 @@
 $(function() {
     $("#lighthouse").hover(
         function() {
-            $(this).attr("src", "Art Page/BTS/Lighthouse_bts.gif");
+            $(this).attr("src", "assets/img/art/bts/Lighthouse_bts.gif");
         },
         function() {
-            $(this).attr("src", "Art Page/Lighthouse.jpg");
+            $(this).attr("src", "assets/img/art/Lighthouse.jpg");
         }
     );
 
     $("#chinese_artwork").hover(
         function() {
-            $(this).attr("src", "Art Page/BTS/Chinese_Artwork_bts.gif");
+            $(this).attr("src", "assets/img/art/bts/Chinese_Artwork_bts.gif");
         },
         function() {
-            $(this).attr("src", "Art Page/Chinese_Artwork.jpg");
+            $(this).attr("src", "assets/img/art/Chinese_Artwork.jpg");
         }
     );
 });
