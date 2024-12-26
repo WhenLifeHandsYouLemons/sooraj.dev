859134413be299289b3b4dcdb469742026495442
Sooraj Sannabhadti <2005sooraj@gmail.com>
2024-12-26 01:58:20 -0800

    Added new artworks

--- assets/js/art-transition.js
+++ assets/js/art-transition.js
@@ -16,4 +16,22 @@ $(function() {
             $(this).attr("src", "assets/img/art/Chinese_Artwork.jpg");
         }
     );
+
+    $("#steaming_cup_anim").hover(
+        function() {
+            $(this).attr("src", "assets/img/art/bts/Steaming_Cup_Anim_bts.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/img/art/Steaming_Cup_Anim.gif");
+        }
+    );
+
+    $("#book_loading_anim").hover(
+        function() {
+            $(this).attr("src", "assets/img/art/bts/Book_Loading_Anim_bts.gif");
+        },
+        function() {
+            $(this).attr("src", "assets/img/art/Book_Loading_Anim.gif");
+        }
+    );
 });
